8ec22cf220696e5b886732d8d5e77eba946c2fcb
marcos-burghasuen <marcos_burghasuen@sicredi.com.br>
2022-04-08 17:10:03 -0300

    validando formulario de cadastro

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const campos = document.querySelectorAll("[required]")     //procure em todos os elementos que tenha o atributo [riquired] e amazene na variavel campos
+
+for(let campo of campos) {    //para cada campo dentro de campos
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelector('.formularioDeRegistro').addEventListener("submit", envent => {    //procurar dentro do documento com class formulario de registro, adicionar um evento, "submit", quando acontecer o submit chamar a função envent
+
+})
