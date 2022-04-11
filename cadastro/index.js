e9289ab55465ee4e04238695808538084d0b6dc6
marcos-burghausen <marcos_burghausen@sicredi.com.br>
2022-04-11 15:56:57 -0300

    adicionado icones no formulario e mensagem de erro

--- cadastro/index.js
+++ cadastro/index.js
@@ -1,35 +1,28 @@
 
-const campos = document.querySelectorAll("[required]")     //procure em todos os elementos que tenha o atributo [riquired] e amazene na variavel campos
+let formulario = document.querySelector(".formularioDeRegistro")
 
-for(let campo of campos) {    //para cada campo dentro de campos
+let nome = document.querySelector("#nome")
 
-}
+let dataNascimento = document.querySelector("#nascimento")
 
+let senha= document.querySelector("#senha")
 
+let senha2= document.querySelector("#senha2")
 
+let email = document.querySelector("#email")
 
+let cpf = document.querySelector("#cpf")
 
+let telefone = document.querySelector("#telefone")
 
+let profissao = document.querySelector("#profissao")
 
+let renda = document.querySelector("#renda")
 
+let cep = document.querySelector("#cep")
 
+let logradouro = document.querySelector("#logradouro")
 
+let cidade = document.querySelector("#cidade")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.querySelector('.formularioDeRegistro').addEventListener("submit", envent => {    //procurar dentro do documento com class formulario de registro, adicionar um evento, "submit", quando acontecer o submit chamar a função envent
-
-})
+let estado = document.querySelector("#estado")
