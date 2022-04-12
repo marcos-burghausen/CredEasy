e82698f19e740cec9093e6b77d7250b91422ad2c
marcos-burghausen <marcos_burghausen@sicredi.com.br>
2022-04-12 16:31:26 -0300

    validação da data com js

--- cadastro/index.js
+++ cadastro/index.js
@@ -1,28 +1,23 @@
 
-let formulario = document.querySelector(".formularioDeRegistro")
+//validação data de nascimento//
+const dataNascimento = document.querySelector('#nascimento')
+dataNascimento.addEventListener('blur', (evento) => {     //adicionando o evento blur que quer dizer quando o elemento perder o foco, pasando o evento//
+    validaDataNascimento(evento.target)     //chamando a funçaõ e passando o evento//
+})
+ function validaDataNascimento(input) {
+     const dataRecebida = new Date(input.value)     //armazenando a data passada no input//
+     let mensagem = ''
+     if(!maiorQue18(dataRecebida)) {             //se a funçao maiorQue18 for false ou seja se o usuadrio for de menor//
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'    //exibe essa mensagem//
+     }
+     input.setCustomValidity(mensagem)      //se for valida exibe a let mensagem que esta vazia//
+ }
 
-let nome = document.querySelector("#nome")
+ function maiorQue18(data) {    // a (date) vai ser a data informada pelo usuario
+     const dataAtual = new Date()   //deichando o new Date em branco ele pega a data atual//
+     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())     //pegando a data informada pelo usuario e somando 18//
 
-let dataNascimento = document.querySelector("#nascimento")
+     return dataMais18 <= dataAtual     //coparando se a dataMais18 e menor ou igual a dataAtual//
+ }
 
-let senha= document.querySelector("#senha")
 
-let senha2= document.querySelector("#senha2")
-
-let email = document.querySelector("#email")
-
-let cpf = document.querySelector("#cpf")
-
-let telefone = document.querySelector("#telefone")
-
-let profissao = document.querySelector("#profissao")
-
-let renda = document.querySelector("#renda")
-
-let cep = document.querySelector("#cep")
-
-let logradouro = document.querySelector("#logradouro")
-
-let cidade = document.querySelector("#cidade")
-
-let estado = document.querySelector("#estado")
